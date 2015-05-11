88558536e30a744a9ecdbad982ae3ee9b57e0e15
root <root@PI201505051434.(none)>
2015-05-11 20:02:02 +0000

    opt dir

---
+++ appnode/scripts/appnode_install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -f /etc/init.d/docker ]; then
+  echo "appnode install: Docker is installed."
+else
+  echo "appnode install: Docker is NOT present. Please install Docker 1.5 or greater."
+fi
+
+echo "Installing/Upgrading puppet ..."
+apt-get install puppet
+if [ -f /etc/puppet/puppet.conf ];then
+  mv /etc/puppet/puppet.conf /etc/puppet/puppet.conf.original
+  ln -s /opt/appnode/etc/puppet/puppet.conf /etc/puppet/puppet.conf
+else
+  echo "appnode install error: file /etc/puppet/puppet.conf not preset"
+  exit 3
+fi
+
+if [ ! -f /etc/init.d/appnode_init ];then
+  ln -s /opt/appnode/etc/init.d/appnode_init /etc/init.d/appnode_init
+fi
+
+
