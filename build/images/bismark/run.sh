70e6543456a2af862b0cbefaebc972c26fbcac4d
gmartins <ggmartins@gmail.com>
2015-05-15 18:54:15 +0000

    init.sh remove

--- build/images/bismark/run.sh
+++ build/images/bismark/run.sh
@@ -16,6 +16,17 @@ echo "Running daemons ..." > /tmp/log/run.log
 touch /etc/cron.d/cron-bismark-mgmt
 /usr/sbin/dropbear -p 2222 -P /var/run/dropbear_port2222.pid -s -g -d /etc/bismark/dropbear/ssh_host_dss_key -r /etc/bismark/dropbear/ssh_host_rsa_key
 
+# Init BISmark ID for the device
+if [ ! -e /etc/bismark/ID.init ];then
+  if [ -e /etc/bismark/ID ];then
+    mv /etc/bismark/ID /etc/bismark/ID.init
+  else
+    touch /etc/bismark/ID.init
+  fi
+  echo PI$(date +%y%m%d%H)$(awk 'BEGIN {srand(); printf "%04X\n", rand()*65535}') > /etc/bismark/ID
+fi
+
+
 trap termination SIGINT
 
 while true
