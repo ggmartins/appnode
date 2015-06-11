b741751a4addb1677649b643f5c4396120edf72d
gmartins <ggmartins@gmail.com>
2015-06-11 13:29:33 -0400

    setting saltstack as default cms

--- appnode/scripts/appnode_install.sh
+++ appnode/scripts/appnode_install.sh
@@ -5,6 +5,10 @@ if [ “$(id -u)” != “0” ]; then
   exit 1
 fi
 
+echo "Installing CMS SaltStack..."
+echo "deb http://debian.saltstack.com/debian wheezy-saltstack main" > /etc/apt/sources.list.d/salt.list
+apt-get update
+apt-get install -qy salt-minion
 
 if [ -f /etc/init.d/docker ]; then
   echo "appnode install: Docker is installed."
@@ -12,7 +16,7 @@ else
   echo "appnode install: Docker is NOT present. Please install Docker 1.5 or greater."
 fi
 
-echo "Installing support for CMS..."
+echo "Installing appnode support for CMS..."
 
 if [ ! -f /etc/init.d/appnode_init ];then
   ln -s /opt/appnode/etc/init.d/appnode_init /etc/init.d/appnode_init
@@ -24,7 +28,7 @@ if [ -f /etc/salt/minion ];then
   /etc/init.d/salt-minion restart
   exit 0
 else
-  echo "appnode install error: file /etc/puppet/puppet.conf not preset"
+  echo "appnode install error: file /etc/salt/minion not preset"
   exit 3
 fi
 
